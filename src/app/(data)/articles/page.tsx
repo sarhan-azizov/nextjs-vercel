9ee7c93420b95ee7447497635afe06bcb08d034b
Serhii Azizov <serhii.azizov@lightspeedhq.com>
2025-02-26 13:10:13 -0500

    initial commit

--- src/app/(data)/articles/page.tsx
+++ src/app/(data)/articles/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import styles from "../../page.module.css";
 
 export default function Page() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [articles, _] = useState(["1","2"]);
     return (
         <div className={styles.page}>
